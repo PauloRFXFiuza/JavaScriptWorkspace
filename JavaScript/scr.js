099902ebac72537df17f000e39e3c0d70c92887c
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-12-16 09:51:48 -0300

    v2c12

--- JavaScript/scr.js
+++ JavaScript/scr.js
@@ -5,8 +5,14 @@ alert('Hi! What´s up!');
 document.getElementById("text").innerHTML="Hi! <b> How are you?";
 console.log("Hi!");
 */
-/* 03 */
+/* 03 
 let a,b,c; a=5,b=6,c=7;
-function f(){document.write(a);document.write(b);}
-
-document.write(f());
+function f() { document.write(a); document.write(b); }
+document.write( f() );
+*/
+/* 04 */
+console.log(5+5); console.log("5+5");
+var a= 1, b=5, c= a+b, d= a*b, e= a/b, f= a-b ;
+console.log(c,d,e,f);
+var person= "John", PERSON= "Lennon"; 
+console.log(PERSON); console.log(person);
