b93504b120bb225787cc8d45cb7cf098af73ad3b
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-12-16 10:54:12 -0300

    v2c17

--- JavaScript/scr.js
+++ JavaScript/scr.js
@@ -19,8 +19,8 @@ console.log( PERSON ); console.log( person ) ;
 */
 /* 05 
 //Hi! /*Hi*/ 
-// 06 
-/*
+/* 06 
+
 var pote= "Bombom" ; alert( pote ) ;
 var a= 2 , b= 3 , c= a + b ; alert( c ) ;
 
@@ -57,7 +57,7 @@ let y= 10; //aqui y = 10
 }
 
 document.getElementById("text2").innerHTML= y; //aqui y = 10
-*/
+
 
 // No const, não é possível nem redeclaração de variável nem reatribuição de valores, conforme mostrado:
 
@@ -65,6 +65,7 @@ document.getElementById("text2").innerHTML= y; //aqui y = 10
 //const a= 3; console.log( a ) ; 
 // Impressão no console - Uncaught SyntaxError: Identifier 'a' has already been declared (at scr.js:65:7)
 
-const a= 2; console.log( a ) ;
-a= 3; console.log( a ) ;
+//const a= 2; console.log( a ) ;
+//a= 3; console.log( a ) ;
 // Impressão no console - scr.js:69 Uncaught TypeError: Assignment to constant variable. at scr.js:69:2
+*/
