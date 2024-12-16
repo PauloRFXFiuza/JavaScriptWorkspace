c53e8bb878e9d9c627a3b29f0ebb894f4269ea2a
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-12-16 09:39:56 -0300

    v2c11

--- JavaScript/scr.js
+++ JavaScript/scr.js
@@ -5,3 +5,8 @@ alert('Hi! What´s up!');
 document.getElementById("text").innerHTML="Hi! <b> How are you?";
 console.log("Hi!");
 */
+/* 03 */
+let a,b,c; a=5,b=6,c=7;
+function f(){document.write(a);document.write(b);}
+
+document.write(f());
