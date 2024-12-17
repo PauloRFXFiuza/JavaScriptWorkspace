cbaae1b1fa3d0f26361f4f3d936d07bd1a2ed003
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-12-17 10:23:41 -0300

    v3c16

--- JavaScript/scr.js
+++ JavaScript/scr.js
@@ -222,6 +222,32 @@ document.getElementById( "text" ).innerHTML = carro.completoHTML();
 console.log( carro.completoConsole() )
 //carro.buzina() ;
 */
-/* 10 
+/* 10 */ 
+/*
+EVENTOS*
+Eventos são ações disparadas pela interação dos usuários na página.
+É o correto manejo desses eventos que tornam as páginas interativas e dinâmicas.
+
+Existem muitos eventos. Veja os mais utilizados:
+
+onclick -> Disparado quando recebe um click.
+ondblclick -> Disparado quando clique duplo.
+onmouseover -> Disparado quando o mouse está sobre.
+onmouseout -> Disparado quando o mouse é movido para fora do elemento.
+onmousemove -> Disparado quando o mouse é movido no elemento.
+onmousedown -> Disparado quando o clique do botão foi pressionado.
+onmouseup -> Disparado quando o clique do botão é liberado.
+onfocus -> Disparado quando o elemento recebe o foco. Válido para input, s
+onchange -> Disparado quando existe uma mudança no conteúdo. "Ao mudar".
+onblur -> Disparado quando o elemento perde o foco.
+onkeydown -> Disparado quando uma tecla é pressionada.
+onkeypress -> Disparado quando uma tecla é pressionada e solta.
+onkeyup -> Disparado quando uma tecla é solta sobre um elemento.
+onload -> Disparado quando a página terminou de ser carregada. Body.
+onresize -> Disparado quando há um redimensionamento da janela.
 a=1;
 */
+
+function eventoClick() {
+    alert( "Evento de clique acionado com sucesso!" ) ;
+}
