64b84fafaeaea95f20f768bc04cc9a018bb07b6f
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-12-19 11:59:33 -0300

    v4c40

--- JavaScript/scr.js
+++ JavaScript/scr.js
@@ -391,8 +391,10 @@ console.log(listaTotal);
 lista3 = ["alcachofra","côco",1,2,3,4,5];
 listaTotal = lista.concat(lista2, lista3); //Comando para juntar varias arrays
 console.log(listaTotal);
-//                        [0]       [1]         [2]             [3]         [4]     [5]     [6]     [7]         [8]             [9]
-let footballPlayers = ["Rooney","Cannavaro","James Rodriguez","Ronaldo","Vini Jr","Messi","Guti","Ribamar","Jonathan Cafú","Freedy Adu"];
+//                        [0]       [1]         [2]             [3]         [4]     [5]     [6]     [7]         [8]        [9]
+let footballPlayers = ["Rooney","Cannavaro","James Rodriguez","Ronaldo","Vini Jr","Messi","Modric","Kross","Neymar Jr","Rodrigo Garro"];
 console.log(footballPlayers);
-let bestFootballPlayers = footballPlayers.slice(3,6);// .slice(N,M) - elimina os itens de uma array antes do índice N e a partir do índice M 
+let bestFootballPlayers = footballPlayers.slice(3);// .slice(N) - elimina os itens de uma array antes do índice N
 console.log(bestFootballPlayers);
+let goatFootballPlayer = footballPlayers.slice(5,6);// .slice(N,M) - elimina os itens de uma array antes do índice N e a partir do índice M 
+console.log(goatFootballPlayer);
