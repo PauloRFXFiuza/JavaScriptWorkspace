e1b8e7339bbe4a4e133a057308a104b0f93c657c
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-12-20 12:27:37 -0300

    v4c75

--- JavaScript/scr.js
+++ JavaScript/scr.js
@@ -534,6 +534,6 @@ function calculaTabuada () {
 }
 */
 
-for (let i=1850; i<=2024; i++){
+for (let i=2024; i>=1850; i--){
     document.getElementById("ano").innerHTML += " <option value='"+i+"'> " +i+"</option>";
 }
