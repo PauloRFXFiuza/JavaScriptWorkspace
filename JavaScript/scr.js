b34a7152a9fed0cdf348d6c5df2a5d1f1f7df19a
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-12-20 08:52:36 -0300

    v4c49

--- JavaScript/scr.js
+++ JavaScript/scr.js
@@ -443,6 +443,14 @@ var hour = new Date().getHours();
 var minutes = new Date().getMinutes();
 console.log("Agora são "+hour+" horas e "+minutes+" minutos.\n");
 
+if (hour<12) {
+    console.log("Bom dia!");
+} else if (hour<18) {
+    console.log("Boa tarde!");
+} else {
+    console.log("Boa noite!");
+}
+    
 
 
 
