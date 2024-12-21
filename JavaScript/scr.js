4e236539e9bf75dbae80963bd99f689cd1f8638d
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-12-21 16:37:50 -0300

    v5c11

--- JavaScript/scr.js
+++ JavaScript/scr.js
@@ -598,3 +598,18 @@ function parar(){
     
 };
 */
+/* 17 */
+/* CLASSES EM JAVASCRIPT
+
+Em 2015 foi introduzido no JavaScript as Classes. As classes 
+são um conceito antigo em programação e várias linguagens utilizam elas.
+ Mas no JavaScript isso é relativamente novo, por isso mesmo programadores 
+ experientes não sabem muito bem utilizar.
+
+Basicamente, as classes são como "fábricas" para criar objetos. 
+Pode se dizer que são "funções especiais" para criação de objetos.
+
+Assim como uma fábrica da vida real precisa das máquinas para 
+construir os objetos, as Classes no JavaScript usam um método 
+chamado constructor() para fabricar os objetos.
+*/
