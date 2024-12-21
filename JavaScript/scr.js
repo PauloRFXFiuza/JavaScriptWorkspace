34299bb204cf7d692ba1ecd963a69aafd4b73c39
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-12-21 14:49:39 -0300

    v5c6

--- JavaScript/scr.js
+++ JavaScript/scr.js
@@ -562,13 +562,14 @@ setInterval(function, milliseconds)
  */
 function ativarContagem(){
     document.getElementById("tempo").innerHTML = "Começou a contar!";
-    setTimeout(function(){
-        document.getElementById("tempo").innerHTML = 
-        "Executou programa de contagem!";
-        document.getElementById("tempo").style.backgroundColor = "blue";
-        document.getElementById("tempo").style.borderRadius = "15px 5px 5px 5px";
-    }, 3000);
+    tempo = setTimeout(function(){
+                document.getElementById("tempo").innerHTML = 
+                "Executou programa de contagem!";
+                document.getElementById("tempo").style.backgroundColor = "blue";
+                document.getElementById("tempo").style.borderRadius = "15px 5px 5px 5px";
+                }, 3000);
 };
 function pararContagem(){
-
+    clearTimeout(tempo);
+    document.getElementById("tempo").innerHTML = "Contagem Encerrada!";
 };
