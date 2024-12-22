7b7fabf271f898eacd5c0646c40631c928ad59dd
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-12-22 10:05:35 -0300

    v5c32

--- JavaScript/scr.js
+++ JavaScript/scr.js
@@ -737,3 +737,22 @@ diferenca = Math.ceil((dataFinal - dataInicial)/(24*60*60*1000));
 console.log(diferenca);
 */
 /* 19 */
+
+/* 
+JSON 
+
+JSON significa JavaScript Object Notation que traduzido pro 
+português fica algo como notação de objeto JavaScript.
+Explicando de um modo simples JSON é basicamente uma forma de 
+converter um objeto em texto e o contrário também, 
+um texto em um objeto.
+Ele é usado principalmente para transmitir dados entre 
+sistemas de forma simples, já que o formato de texto é lido 
+por praticamente toda linguagem de programação.
+
+Para trabalhar com JSON no Javascript usamos dois métodos:
+
+JSON.parse() -> Converte texto no padrão JSON em objetos
+JSON.stringify() -> Converte objetos em texto padrão JSON
+*/
+
