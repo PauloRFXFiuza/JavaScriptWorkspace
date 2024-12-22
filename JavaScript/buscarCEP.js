5a66d61dd18e552d4296657094b6f5e7fc5a51d1
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-12-22 12:24:13 -0300

    v6c3

--- JavaScript/buscarCEP.js
+++ JavaScript/buscarCEP.js
@@ -1,37 +1,64 @@
 //Data e horário completo de hoje
-var data = new Date();
+const data = new Date();
 //Dia do Mês
-var diaDoMes= data.getDate();
+const diaDoMes= data.getDate();
 //Mês Atual- Formato numérico- 0= janeiro,1=fevereiro,...,11=dezembro
-var mes = data.getMonth();
+const mes = data.getMonth();
 //Mês Atual no formato literal em português
-var mesesDoAno = ["Janeiro", "Fevereiro", "Março","Abril", "Maio", "Junho", "Julho",
+const mesesDoAno = ["Janeiro", "Fevereiro", "Março","Abril", "Maio", "Junho", "Julho",
                     "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
-var mesLiteral = mesesDoAno[data.getMonth()];
+const mesLiteral = mesesDoAno[data.getMonth()];
 //Ano atual com 4 dígitos
-var ano = data.getFullYear();
+const ano = data.getFullYear();
 //Dia da Semana- Formato numérico- 0=domingo,1=segunda,...,6-sábado
-var diaDaSemana = data.getDay();
+const diaDaSemana = data.getDay();
 //Dia da Semana no formato literal em português
-var diasDaSemana = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira",
+const diasDaSemana = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira",
                     "Quinta-Feira", "Sexta-Feira", "Sábado"];
-var diaLiteral = diasDaSemana[data.getDay()];
+const diaLiteral = diasDaSemana[data.getDay()];
 
-var horarioLocal = new Date().toLocaleTimeString();
-var dataDeHoje = new Date().toDateString();
+const horarioLocal = new Date().toLocaleTimeString();
+const dataDeHoje = new Date().toDateString();
 //Imprimir Data e Horário no cabeçalho da Página
 document.getElementById("data").innerHTML="<strong>Hoje é:</strong> "
 +diaLiteral+", "+diaDoMes+" de "+mesLiteral+" de "+ano+"."
 + "<br><strong>Horário de Brasília:</strong> "+horarioLocal;
 
 function buscarCEP() {
-    let input = document.getElementById('cep').value;
+    const input = document.getElementById('cep').value;
 
     const ajax = new XMLHttpRequest();
     ajax.open('GET', 'https://viacep.com.br/ws/' + input + '/json/');
     ajax.send();
 
     ajax.onload = function () {
-        document.getElementById('texto').innerHTML = this.responseText;
+        console.log(this.responseText);
+        //Conversão texto JSON em objeto
+        const objeto= JSON.parse(this.responseText);
+        //Recuperando alguns elementos do objeto
+        const endereco= [objeto.cep, objeto.logradouro, objeto.complemento,
+        objeto.bairro, objeto.localidade, objeto.estado, objeto.uf,
+        objeto.regiao, objeto.ddd];
+        //Imprimindo os resultados na página HTML
+        if (endereco[0] === undefined){
+            document.getElementById("texto").innerHTML = 
+            "<strong>O CEP digitado não existe.<br>Favor clicar no botão "+ 
+            "Nova Consulta para realizar uma nova consulta de CEP.</strong>"    
+        }else{
+            document.getElementById("texto").innerHTML =
+            "<strong>CEP:</strong> "+endereco[0]+
+            "<br><strong>Logradouro:</strong> "+endereco[1]+
+            "<br><strong>Complemento:</strong> "+endereco[2]+
+            "<br><strong>Bairro:</strong> "+endereco[3]+
+            "<br><strong>Município:</strong> "+endereco[4]+
+            "<br><strong>Estado:</strong> "+endereco[5]+
+            " ("+endereco[6]+")"+
+            "<br><strong>Região:</strong> "+endereco[7]+
+            "<br><strong>DDD:</strong> "+endereco[8];
+        }
     }
+}
+function novaConsulta() {
+    //Atualizar a Página
+    location.reload();
 }
